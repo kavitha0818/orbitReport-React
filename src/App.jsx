536341735cd8cb8f0c67c5f4edced33bcd5212b7
kavitha1808 <kavitha.sekarp@gmail.com>
2024-05-09 11:15:24 -0500

    Orbit report

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import {useState} from "react";
+import satData from "./components/satData";
 
-
+//create the variables in the app funtion
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
+  const filterByType = (currentType) => {
+    const filteredSats = satData.filter((newSatDisplay) => {
+       return newSatDisplay.orbitType === currentType;
+    });
+    setSat(filteredSats);
+ };
+ 
   return (
-    <div>
-      <Buttons />
-      <Table />
-    </div>
+    <>
+      <Banner />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        orbitTypes={displaySats}
+      />
+      <Table sat={sat} />
+    </>
   );
 }
 
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,20 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
+import React from "react";
+import satData from "./satData";
+import './styling.css'
+
+const Buttons = ({ filterByType, setSat, orbitTypes }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className = 'flex-container'>
+      {orbitTypes.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+
     </div>
   );
 };
